c3fdd17ad47e13c00168408f4790cd516b6cb782
Drake <drakeaxelrod@gmail.com>
2020-09-27 19:55:35 +0200

    fixing Navya's customer section

--- {code}pendent/src/DartSystem/Customer.java
+++ {code}pendent/src/DartSystem/Customer.java
@@ -1,127 +1,112 @@
 package DartSystem;
 
-import java.util.Scanner;
-import java.util.UUID;
-
 public class Customer {
 
-private String customerId;
-private String firstName;
-private String lastName;
+    private int customerId;
+    private String name;
+    Helper helper=new Helper();
 
-private static Customer[] customerList={ new Customer("ver","Vernita", "hdkjn"),
-        new Customer("nav","Navya","pulikanla"),
-        new Customer("Dra","Drake","dr"),
-        new Customer("Alt","Altan","alt"),
-        new Customer("Ax","Axel","axe")};
+    private static Customer[] customerList={ new Customer(1,"Vernita"),
+            new Customer(2,"Navya"),
+            new Customer(3,"Drake"),
+            new Customer(4,"Altan"),
+            new Customer(5,"Axel")};
 
     // Default Constructor
-     Customer() {
+    Customer() {
     }
-   Customer(String customerId,String firstName,String lastName){
-         this.customerId=customerId;
-         this.firstName=firstName;
-         this.lastName=lastName;
-         }
 
+    Customer(int customerId,String name){
+        this.customerId=customerId;
+        this.name=name;
 
-    /*public static void customerMenu() {
-        // TODO make not static
-        System.out.println("\nCustomer Screen - Type one of the options below:");
-        System.out.println("1. Rent a game");
-        System.out.println("2. Return a game");
-        System.out.println("3. Return to Main Menu");
-
-        String[] customerMenuAcceptSet = {"1", "2", "3"}; // Accepted responses for menu options
-        Helper input = new Helper(); // Creating new Helper object
-        String mInput = input.getMenuInput("Please enter your option: ", customerMenuAcceptSet); // Calling Helper method
-
-        switch(mInput.toLowerCase())
-        {
-            case "1":
-                System.out.println("Looking for a game?");
-                Rental toRent=new Rental();
-                toRent.rentGame();
-                break;
-            case "2":
-                System.out.println("You took it a year ago?!");
-                Rental toReturn=new Rental();
-                    toReturn.returnGame();
-                break;
-            case "3":
-                DartController.DartController();
-                break;
-            default:
-                System.out.println("no match");
-        }
-    }*/
+    }
 
     public String toString(){
-         return
-      "ID :"+ this.customerId+" : "+ this.firstName+" "+this.lastName;
+        return
+                "ID :"+ this.customerId+ " : "+this.name;
     }
-   public void IncreaseArray() {
-       Customer[] customerListNew = new Customer[customerList.length + (customerList.length / 2)];
-       for (int i = 0; i < customerList.length; i++) {
-           customerListNew[i] = customerList[i];
-       }
-       customerList = customerListNew;
-       }
+    public void IncreaseArray() {
+        Customer[] customerListNew = new Customer[customerList.length + (customerList.length / 2)];
+        for (int i = 0; i < customerList.length; i++) {
+            customerListNew[i] = customerList[i];
+        }
+        customerList = customerListNew;
+    }
     public void addCustomer() {
+        int countArray = 0;
+        for(int i = 0; i < customerList.length; i++){
+            if(customerList[i] != null){
+                countArray++;
+            }
+        }
+
+        int countId = customerList[countArray - 1].customerId + 1;
+
         if (customerList[customerList.length - 1] != null) {
             IncreaseArray();
         }
-        // TODO make not static
-        Scanner newCustomer = new Scanner(System.in);
+
         System.out.println("Add a customer here");
+        System.out.println("Suggested ID: " + countId);
         System.out.print("Enter the customers ID : ");
-        String cusIdInputs = newCustomer.nextLine();
-        System.out.print("Enter the customers first name: ");
-        String cusFirstName = newCustomer.nextLine();
-        System.out.print("Enter the customers last name: ");
-        String cusLastName = newCustomer.nextLine();
+        int cusIdInputs = helper.input.nextInt();
+        helper.input.nextLine();
+        System.out.print("Enter the customers  name: ");
+        String cusName = helper.input.nextLine();
         int arrayCount = 0;
         for (int i = 0; customerList[i] != null; i++) {
             arrayCount = i + 1;
         }
-        customerList[arrayCount] = new Customer(cusIdInputs, cusFirstName, cusLastName);
+        customerList[arrayCount] = new Customer(cusIdInputs, cusName);
         System.out.println("Customer added successfully :" + customerList[arrayCount].toString());
         System.out.print("If you want to add another customer press '1': ");
-        int anotherEntry = newCustomer.nextInt();
-      if (anotherEntry == 1) {
+        int anotherEntry = helper.input.nextInt();
+        if (anotherEntry == 1) {
             addCustomer();
         }
         else if (anotherEntry!=1){
-                System.out.print("Invaid,customer can't be added");
-                //employeeMenu();
-           // addCustomer();
+            System.out.print("Invalid entry ");
+            EmployeeMenu.employeeMenu();
         }
-        newCustomer.close();
     }
 
 
-    public static void removeCustomer() {
-        // TODO make not static
-        Scanner remCustomer = new Scanner(System.in);
+    public void removeCustomer() {
         System.out.println("Remove a customer here by entering their ID : ");
-        String cusIdToRemove = remCustomer.nextLine();
-
+        int cusIdToRemove = helper.input.nextInt();
 
         for (int i = 0; i < customerList.length; i++) {
-            if (customerList[i].customerId.equalsIgnoreCase(cusIdToRemove)) {
+            if (customerList[i].customerId == cusIdToRemove) {
                 for (int j = i + 1; j < customerList.length; j++) {
                     customerList[i] = customerList[j];
-                    System.out.println(customerList[i].toString());
-                }}else{
-                    System.out.println("Invalid customer Id");
-                    //employeeMenu();
+                    i++;
                 }
-
-
-
+                int arrayCount = 0;
+                for (int k = 0; k < customerList.length; k++){
+                    if (customerList[k] != null){
+                        arrayCount++;
+                    }
+                }
+                customerList[arrayCount-1] = null;
             }
         }
-
+        System.out.println("Customer Removed");
+        for (int i = 0; i < customerList.length; i++) {
+            if (customerList[i]!=null){
+                System.out.println(customerList[i].toString());
+            }
+        }
+        EmployeeMenu.employeeMenu();
+    }
 }
+
+
+
+
+
+
+
+
 
 
